b0a270c5ceadcf0f032cbc9351de9c64ebba2913
Jakub Zwolakowski <jakub.zwolakowski@trust-in-soft.com>
2021-09-24 18:01:32 +0200

    typo

--- CERT_C/FIO/FIO46-C/example_noncompliant.c
+++ CERT_C/FIO/FIO46-C/example_noncompliant.c
@@ -17,5 +17,5 @@ int main(void) {
 
 // NOT DETECTED
 // CMD: tis-analyzer --interpreter test_FIO46-C_noncompliant.c
-// C17: https://cigix.me/c17#6.2.4
+// C17: https://cigix.me/c17#7.21.3
 // UB: "The value of a pointer to a FILE object is used after the associated file is closed"
