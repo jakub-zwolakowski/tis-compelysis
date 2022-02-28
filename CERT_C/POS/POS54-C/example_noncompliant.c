651e7b65179e2a846a35455fa480340157a32eea
Jakub Zwolakowski <jakub.zwolakowski@trust-in-soft.com>
2022-02-28 18:10:40 +0100

    POS54-C

---
+++ CERT_C/POS/POS54-C/example_noncompliant.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+  
+int main(int argc, char *argv[]) {
+  FILE *out;
+  FILE *in;
+  size_t size;
+  char *ptr;
+  
+  if (argc != 2) {
+    /* Handle error */
+    return 1;
+  }
+  
+  in = fmemopen(argv[1], strlen(argv[1]), "r");
+  /* Use in */
+  
+  out = open_memstream(&ptr, &size);
+  /* Use out */
+  
+  return 0;
+}
+
+// NOT DETECTED
+// CMD: tis-analyzer --interpreter -val-args=" 42" example_noncompliant.c
+// C17: ?
+// UB: ?
