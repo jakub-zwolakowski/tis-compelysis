e86f8f154f0451f2707b87d01ab4b80833c8599a
Jakub Zwolakowski <jakub.zwolakowski@trust-in-soft.com>
2022-03-03 11:47:33 +0100

    CON40-C

---
+++ CERT_C/CON/CON40-C/example_compliant.md
@@ -0,0 +1,33 @@
+## example_compliant
+### Test
+```
+flag = true
+```
+### gcc
+```
+```
+### clang
+```
+```
+### UBSan
+```
+flag = true
+```
+### Valgrind
+```
+==12345== Memcheck, a memory error detector
+==12345== Copyright (C) 2002-2017, and GNU GPL'd, by Julian Seward et al.
+==12345== Using Valgrind-3.13.0 and LibVEX; rerun with -h for copyright info
+==12345== Command: ./example_compliant.out
+==12345== 
+flag = true
+==12345== 
+==12345== HEAP SUMMARY:
+==12345==     in use at exit: 0 bytes in 0 blocks
+==12345==   total heap usage: 6 allocs, 6 frees, 123,456 bytes allocated
+==12345== 
+==12345== All heap blocks were freed -- no leaks are possible
+==12345== 
+==12345== For counts of detected and suppressed errors, rerun with: -v
+==12345== ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)
+```
---
+++ CERT_C/CON/CON40-C/example_noncompliant.c
@@ -0,0 +1,51 @@
+#include <stdatomic.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#ifdef C11_THREADS
+#include <thread.h>
+#else
+#include "../c11threads.h"
+#endif
+
+enum { NTHREADS = 5 };
+
+static atomic_bool flag = ATOMIC_VAR_INIT(false);
+   
+void init_flag(void) {
+  atomic_init(&flag, false);
+}
+   
+void toggle_flag(void) {
+  bool temp_flag = atomic_load(&flag);
+  temp_flag = !temp_flag;
+  atomic_store(&flag, temp_flag);
+}
+     
+bool get_flag(void) {
+  return atomic_load(&flag);
+}
+
+int my_thread(void *arg) {
+  toggle_flag();
+  return 0;
+}
+
+int main(void) {
+  thrd_t threads[NTHREADS];
+  for (size_t i = 0; i < NTHREADS; i++) {
+    thrd_create(&threads[i], my_thread, NULL);
+  }
+  for (size_t i = 0; i < NTHREADS; i++) {
+    thrd_join(threads[i], NULL);
+  }
+  bool loaded_flag = atomic_load(&flag);
+  printf("flag = %s\n", loaded_flag ? "true" : "false");
+  return 0;
+}
+
+// NOT DETECTED
+// CMD: tis-analyzer -val -slevel 1000 -mthread -mt-write-races example_noncompliant.c
+// C17: ?
+// UB: ?
+// COMPILE: gcc -lpthread
